3564aded52debd5bdcc7b48f4ff46090f1454674
ronaldmelara <ronaldmelara@gmail.com>
2022-09-23 16:15:05 -0300

    Resolucion ejercicio tema 1

--- ejercicio-tema-1/ejer-tema01/src/Main.java
+++ ejercicio-tema-1/ejer-tema01/src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int num_entero = 1;
+        long num_long = 2555;
+        double num_double = 89.12d;
+        Boolean booleano = true;
+        String str = "Valor string";
+        short num_short = 67;
+        float num_float = 3.14159f;
+        char chr = 'a';
+
+        System.out.println(str);
+        System.out.println(num_entero);
+        System.out.println(num_long);
+        System.out.println(num_double);
+        System.out.println(booleano);
+        System.out.println(num_short);
+        System.out.println(num_float);
+        System.out.println(chr);
+
+        Integer num_obj_int = 100;
+        System.out.println(num_obj_int);
+
+        Long num_obj_long = 5700L;
+        System.out.println(num_obj_long);
     }
 }
